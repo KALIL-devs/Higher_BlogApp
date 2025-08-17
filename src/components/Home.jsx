eefdcd56c5edc1091e97a86c093807232b2a617a
KALIL-devs <kalilrahman055@gmail.com>
2025-08-17 11:40:14 +0530

    Update blog layout and responsive styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function Home() {
   return (
     <>
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-6 p-6 max-w-[1200px] mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6 max-w-[1200px] mx-auto">
         {blogs.map(blog => (
           <BlogPreview key={blog.id} blog={blog} />
         ))}
